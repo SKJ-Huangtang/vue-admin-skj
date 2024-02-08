cd3727b086fb8ff293a02966d47fc10f36d5af26
SKJ-Huangtang <shangkejie0507@gamil.com>
2024-02-08 23:11:00 +0800

    new file:   .editorconfig 	new file:   .env.development 	new file:   .env.production 	new file:   .env.staging 	new file:   .eslintignore 	new file:   .eslintrc.js 	new file:   .gitignore 	new file:   .travis.yml 	new file:   LICENSE 	new file:   README-zh.md 	modified:   README.md 	new file:   babel.config.js 	new file:   build/index.js 	new file:   dist.tar 	new file:   jest.config.js 	new file:   jsconfig.json 	new file:   mock/index.js 	new file:   mock/mock-server.js 	new file:   mock/table.js 	new file:   mock/user.js 	new file:   mock/utils.js 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.vue 	new file:   src/api/table.js 	new file:   src/api/user.js 	new file:   src/assets/404_images/404.png 	new file:   src/assets/404_images/404_cloud.png 	new file:   src/components/Breadcrumb/index.vue 	new file:   src/components/Hamburger/index.vue 	new file:   src/components/SvgIcon/index.vue 	new file:   src/icons/index.js 	new file:   src/icons/svg/dashboard.svg 	new file:   src/icons/svg/example.svg 	new file:   src/icons/svg/eye-open.svg 	new file:   src/icons/svg/eye.svg 	new file:   src/icons/svg/form.svg 	new file:   src/icons/svg/link.svg 	new file:   src/icons/svg/nested.svg 	new file:   src/icons/svg/password.svg 	new file:   src/icons/svg/table.svg 	new file:   src/icons/svg/tree.svg 	new file:   src/icons/svg/user.svg 	new file:   src/icons/svgo.yml 	new file:   src/layout/components/AppMain.vue 	new file:   src/layout/components/Navbar.vue 	new file:   src/layout/components/Sidebar/FixiOSBug.js 	new file:   src/layout/components/Sidebar/Item.vue 	new file:   src/layout/components/Sidebar/Link.vue 	new file:   src/layout/components/Sidebar/Logo.vue 	new file:   src/layout/components/Sidebar/SidebarItem.vue 	new file:   src/layout/components/Sidebar/index.vue 	new file:   src/layout/components/index.js 	new file:   src/layout/index.vue 	new file:   src/layout/mixin/ResizeHandler.js 	new file:   src/main.js 	new file:   src/permission.js 	new file:   src/router/index.js 	new file:   src/settings.js 	new file:   src/store/getters.js 	new file:   src/store/index.js 	new file:   src/store/modules/app.js 	new file:   src/store/modules/settings.js 	new file:   src/store/modules/user.js 	new file:   src/styles/element-ui.scss 	new file:   src/styles/index.scss 	new file:   src/styles/mixin.scss 	new file:   src/styles/sidebar.scss 	new file:   src/styles/transition.scss 	new file:   src/styles/variables.scss 	new file:   src/utils/auth.js 	new file:   src/utils/get-page-title.js 	new file:   src/utils/index.js 	new file:   src/utils/request.js 	new file:   src/utils/validate.js 	new file:   src/views/404.vue 	new file:   src/views/dashboard/index.vue 	new file:   src/views/form/index.vue 	new file:   src/views/login/index.vue 	new file:   src/views/nested/menu1/index.vue 	new file:   src/views/nested/menu1/menu1-1/index.vue 	new file:   src/views/nested/menu1/menu1-2/index.vue 	new file:   src/views/nested/menu1/menu1-2/menu1-2-1/index.vue 	new file:   src/views/nested/menu1/menu1-2/menu1-2-2/index.vue 	new file:   src/views/nested/menu1/menu1-3/index.vue 	new file:   src/views/nested/menu2/index.vue 	new file:   src/views/table/index.vue 	new file:   src/views/tree/index.vue 	new file:   tests/unit/.eslintrc.js 	new file:   tests/unit/components/Breadcrumb.spec.js 	new file:   tests/unit/components/Hamburger.spec.js 	new file:   tests/unit/components/SvgIcon.spec.js 	new file:   tests/unit/utils/formatTime.spec.js 	new file:   tests/unit/utils/param2Obj.spec.js 	new file:   tests/unit/utils/parseTime.spec.js 	new file:   tests/unit/utils/validate.spec.js 	new file:   vue.config.js

---
+++ src/views/nested/menu1/menu1-2/menu1-2-1/index.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <div style="padding:30px;">
+    <button><a href="https://jable.tv/" target="_blank">jav</a></button>
+    <button><a href="https://18comic.org/" target="_blank">韩漫</a></button>
+    <button><a href="https://madou.club/" target="_blank">madou</a></button>
+    <button><a href="www.czzy.tv" target="_blank">厂长影视</a></button>
+    <button><a href="https://www.nivod4.tv/" target="_blank">泥巴影院</a></button>
+    <br><br>
+    <button><a href="https://www.fofoyy.com/" target="_blank">fofoyy影院</a></button>
+    <button><a href="https://www.hanjukankan.com/" target="_blank">韩剧看看</a></button>
+    <button><a href="https://yhdm.one/" target="_blank">樱花动漫</a></button>
+    <button><a href="https://omofun.in/" target="_blank">omofun</a></button>
+    
+  </div>
+</template>
---
+++ src/views/nested/menu1/menu1-2/menu1-2-2/index.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <div style="padding:30px;">
+    <button><a href="https://52.mk/" target="_blank">品云收藏夹</a></button>
+    <button><a href="https://www.paofu.cloud/" target="_blank">泡芙</a></button>
+    <button><a href="https://祢豆子.com/#/dashboard" target="_blank">祢豆子</a></button>
+    <button><a href="https://fengzg.net/#/dashboard" target="_blank">风之谷</a></button>
+    <button><a href="http://v2ky.me/#/dashboard/" target="_blank">poless</a></button>
+    <br /><br /><br />
+    <button><a href="https://nfcloud.one/user" target="_blank">推油nfcloud</a></button>
+    <button><a href="https://kugou2.cloud/#/dashboard" target="_blank">kugou云</a></button>
+    <button><a href="https://weinijichang.com/#/dashboard" target="_blank">维尼云</a></button>
+    <button><a href="https://jk-cloud.net/dashboard" target="_blank">jk-cloud云</a></button>
+    <br /><br /><br />
+    <button><a href="https://matsuridayo.github.io/" target="_blank">sing-box GUI</a></button>
+  </div>
+</template>
